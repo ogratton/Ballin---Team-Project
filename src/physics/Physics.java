116f827b89bf784969f43ccae40f784fb24dddf6
Aaquib-Naved <aaquibnaved@Aaquibs-MacBook-Pro.local>
2017-03-06 11:14:41 +0000

    Fixed a few things.

--- src/physics/Physics.java
+++ src/physics/Physics.java
@@ -92,6 +92,7 @@ public class Physics extends Thread implements ActionListener {
 			if(c.getDyingStep() >= 50) { //the last dyingStep is 50
 				c.decrementLives();
 				resources.getMap().spawn(c);
+				
 			}
 		}
 		
