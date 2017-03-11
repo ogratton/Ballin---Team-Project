3ba2f235a01beb516ad24111d2e852127e5e05bc
oxg558 <oxg558@cs.bham.ac.uk>
2017-03-11 13:15:31 +0000

    Path smoothing done, making the AI more realistic and therefore more suicidal

--- src/ai/pathfinding/PathFindingTest.java
+++ src/ai/pathfinding/PathFindingTest.java
@@ -20,9 +20,24 @@ public class PathFindingTest
 {
 	private static Point startTile = new Point(12, 28);
 	//	private static Point[] destinations = new Point[] { new Point(12, 28), new Point(8, 32), new Point(16, 38), new Point(20, 20) };
-	private static Point[] destinations = new Point[] { new Point(6, 20), new Point(10, 14), new Point(9, 4), new Point(20, 43) }; // TODO to test moveAwayFrom Edge
-//	private static Point[] destinations = new Point[] { new Point(6, 20), new Point(10, 20), new Point(9, 20), new Point(20, 20) }; // TODO to test smoothing
+	//	private static Point[] destinations = new Point[] { new Point(6, 20), new Point(10, 14), new Point(9, 4), new Point(20, 43) }; // TODO to test moveAwayFrom Edge
+	private static Point[] destinations = new Point[] { new Point(6, 20), new Point(10, 20), new Point(9, 20), new Point(20, 20) }; // TODO to test smoothing
 
+	private static boolean followSetPoints = false;
+	
+	private static void testPoirot(Character player, VeryBasicAI ai)
+	{
+		ai.setBehaviour("poirot"); // so we can feed it our own waypoints
+		ai.setDestinations(destinations);
+		player.setAI(ai);
+	}
+	
+	private static void testRoving(Character player, VeryBasicAI ai)
+	{
+		ai.setBehaviour("roving");
+		player.setAI(ai);
+	}
+	
 	public static void main(String[] args)
 	{
 
@@ -52,15 +67,23 @@ public class PathFindingTest
 		Character player = new Character(Character.Class.WIZARD, 0);
 		player.setX(startCoords.getX());
 		player.setY(startCoords.getY());
-		player.setPlayerNumber(0);
+		player.setPlayerNumber(0); // must be 0 otherwise no debug drawn
 		resources.addPlayerToList(player);
 
 		VeryBasicAI ai = new VeryBasicAI(resources, player);
-		ai.setBehaviour("poirot"); // so we can feed it our own waypoints
-		ai.setDestinations(destinations);
+		if (followSetPoints)
+		{
+			testPoirot(player, ai);
+		}
+		else
+		{
+			testRoving(player, ai);
+		}
+		
+		
 
 		/* STARTING THINGS UP */
-		
+
 		DebugMode mode = new DebugMode(resources);
 		mode.start();
 
