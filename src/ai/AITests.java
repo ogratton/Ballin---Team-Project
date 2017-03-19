c192790072a850ee37e3cd2766925cbec82401a4
oxg558 <oxg558@cs.bham.ac.uk>
2017-03-19 21:56:06 +0000

    JUnit and fixing silence bug in PlayGame

---
+++ src/ai/AITests.java
@@ -0,0 +1,115 @@
+package ai;
+
+import static org.junit.Assert.assertEquals;
+
+import java.awt.Point;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import ai.AITemplate.Behaviour;
+import gamemodes.DebugMode;
+import resources.Character;
+import resources.Map;
+import resources.MapCosts;
+import resources.Resources;
+import resources.Map.Tile;
+
+public class AITests
+{
+
+	// TODO make wrappers for the private methods that need testing
+
+	/*
+	 * BasicAI:
+	 * 
+	 * funeral()
+	 * scanForNearestPlayer
+	 * ?? moveToNextWaypoint()
+	 * normalToNextWaypoint()
+	 * getOurLocation()
+	 * getCurrentTileCoords()
+	 * getTileCoords()
+	 * ?? projectedPosition()
+	 * isWalkable()
+	 * convertWaypoints()
+	 * ?? moveTo()
+	 * ?? fuzzyEqual()
+	 * setBehaviour()
+	 * setDestinations()
+	 */
+
+	/*
+	 * AStarSearch
+	 * 
+	 * addNeighbours()
+	 * sparsifyPath()
+	 * smoothPath()
+	 */
+
+	private Resources resources;
+	private Character cpu;
+	private Character controlled;
+	private static Point[] destinations = new Point[] { new Point(12, 28), new Point(8, 32), new Point(16, 38), new Point(20, 20) };
+
+	@Before
+	public void setUp() throws Exception
+	{
+		resources = new Resources();
+		resources.setMap(new Map(1200, 650, Map.World.SPACE, "asteroid"));
+		new MapCosts(resources);
+		
+		// user-controlled character
+		controlled = new Character(Character.Class.HORSE, 1);
+		controlled.setX(400);
+		controlled.setY(400);
+		resources.addPlayerToList(controlled);
+		
+		// cpu
+		cpu = new Character(Character.Class.WIZARD, 0);
+		cpu.setX(500);
+		cpu.setY(520);
+		cpu.setPlayerNumber(0); // so debug stuff is drawn
+		resources.addPlayerToList(cpu);
+
+	}
+
+	/**
+	 * Uses the FightingAI to test AITemplate functions
+	 */
+	@Test
+	public void AITemplateTest()
+	{
+		FightingAI ai = new FightingAI(resources, cpu);
+		cpu.setAI(ai);
+		
+		DebugMode mode = new DebugMode(resources);
+		mode.start();
+
+		ai.start();
+		
+		assertEquals(Tile.FLAT, ai.getCurrentTile());
+		assertEquals(new Point(20,20), ai.getCurrentTileCoords());
+//		System.out.println(ai.distToNearestPlayer());
+		
+		// wait a bit
+		try
+		{
+			Thread.sleep(10000);
+		}
+		catch (InterruptedException e)
+		{
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		assertEquals(ai.currentTarget, controlled);
+		assertEquals(ai.scanForNearestPlayer(), controlled);
+		
+		ai.setBehaviour(Behaviour.POIROT);
+		ai.setDestinations(destinations);
+		
+//		assertEquals();
+		
+	}
+}
--- src/ai/pathfinding/PathFindingJUnit.java
+++ src/ai/pathfinding/PathFindingJUnit.java
@@ -1,4 +1,4 @@
-package ai.junit;
+package ai.pathfinding;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
@@ -6,45 +6,29 @@ import static org.junit.Assert.assertTrue;
 import java.awt.Point;
 import java.util.TreeSet;
 
+import org.junit.Before;
 import org.junit.Test;
 
-import ai.pathfinding.PointComparator;
-import ai.pathfinding.SearchNode;
-import ai.pathfinding.StaticHeuristics;
-import ai.pathfinding.Vector;
+import resources.Resources;
 
-public class AITests
+public class PathFindingJUnit
 {
-
-	// TODO make wrappers for the private methods that need testing
-
-	/*
-	 * BasicAI:
-	 * 
-	 * funeral()
-	 * scanForNearestPlayer
-	 * ?? moveToNextWaypoint()
-	 * normalToNextWaypoint()
-	 * getOurLocation()
-	 * getCurrentTileCoords()
-	 * getTileCoords()
-	 * ?? projectedPosition()
-	 * isWalkable()
-	 * convertWaypoints()
-	 * ?? moveTo() 
-	 * ?? fuzzyEqual()
-	 * setBehaviour()
-	 * setDestinations()
-	 */
-
-	/*
-	 * AStarSearch
-	 * 
-	 * addNeighbours()
-	 * sparsifyPath()
-	 * smoothPath()
-	 */
-
+	
+	private Resources resources;
+	
+	@Before
+	public void setUp() throws Exception
+	{
+		resources = new Resources();
+	}
+	
+	@Test
+	public void aStartTest()
+	{
+		AStarSearch aStar = new AStarSearch(resources);
+		// TODO
+	}
+		
 	/**
 	 * 
 	 * Tests the following functions in StaticHeuristics
@@ -56,7 +40,7 @@ public class AITests
 	@Test
 	public void staticHeuristicsTest()
 	{
-		
+
 		// setup
 		Point a = new Point(0, 0);
 		Point b = new Point(2, 3);
@@ -71,65 +55,59 @@ public class AITests
 		double manAB = StaticHeuristics.manhattan(a, b);
 		assertEquals(manAB_, manAB, 0);
 	}
-	
+
 	/**
 	 * 
 	 * Tests the following functions in Vector
 	 *
-	 * getCentre() 
+	 * getCentre()
 	 * getX()
-	 * getY() 
+	 * getY()
 	 * pointInside()
-	 * equalDirection() 
+	 * equalDirection()
 	 * 
 	 */
 	@Test
 	public void vectorTest()
 	{
 		// setup
-		Point o = new Point(500,500);
-		
-		Vector v0 = new Vector(0.4, -0.6, o);	
+		Point o = new Point(500, 500);
+
+		Vector v0 = new Vector(0.4, -0.6, o);
 		Vector v0n = v0.normal(o);
-		
+
 		Vector v1 = new Vector(-1, 0, o);
 		Vector v1n = v1.normal(o);
-		
+
 		// getCentre()
 		assertEquals(v0.getCentre(), o);
 		assertEquals(v1.getCentre(), o);
-		
+
 		// getX()
 		assertEquals(v0.getX(), 0.4, 0);
 		assertEquals(v1.getX(), -1, 0);
-		
+
 		// getY()
 		assertEquals(v0.getY(), -0.6, 0);
 		assertEquals(v1.getY(), 0, 0);
-		
+
 		// pointInside()
-		boolean[] tests = new boolean[]{
-				v0n.pointInside(new Point(500, 600)),
-				v0n.pointInside(new Point(440, 480)),
-				!v0n.pointInside(new Point(500, 400)),
-				!v0n.pointInside(new Point(600,400)),
-				
-				v1n.pointInside(new Point(600, 600)),
-				v1n.pointInside(new Point(600, 400)),
-				!v1n.pointInside(new Point(400, 400)),
-				!v1n.pointInside(new Point(400, 600))
-				};
-		
+		boolean[] tests = new boolean[] { v0n.pointInside(new Point(500, 600)), v0n.pointInside(new Point(440, 480)),
+				!v0n.pointInside(new Point(500, 400)), !v0n.pointInside(new Point(600, 400)),
+
+				v1n.pointInside(new Point(600, 600)), v1n.pointInside(new Point(600, 400)), !v1n.pointInside(new Point(400, 400)),
+				!v1n.pointInside(new Point(400, 600)) };
+
 		for (int i = 0; i < tests.length; i++)
 		{
 			assertTrue(tests[i]);
 		}
-		
+
 		// equalDirection
 		assertTrue(v0.equalDirection(new Vector(0.4, -0.6, o))); // exactly alike
 		assertTrue(v0.equalDirection(new Vector(0.5, -0.5, o))); // nearly alike
 	}
-	
+
 	/**
 	 * Tests the points are sorted correctly in sorted Collections
 	 * (sorts by x then y ascending)
@@ -140,15 +118,15 @@ public class AITests
 		TreeSet<Point> testSet = new TreeSet<Point>(new PointComparator());
 		Point p1 = new Point(0, 100);
 		Point p2 = new Point(200, 300);
-		Point p3 = new Point(50,500);
+		Point p3 = new Point(50, 500);
 		testSet.add(p1);
 		testSet.add(p2);
 		testSet.add(p3);
-		
-		assertTrue(testSet.higher(new Point(-1,-1)).equals(p1)); // test item considered least
-		assertTrue(testSet.lower(new Point(1000,1000)).equals(p2)); // test item considered greatest
+
+		assertTrue(testSet.higher(new Point(-1, -1)).equals(p1)); // test item considered least
+		assertTrue(testSet.lower(new Point(1000, 1000)).equals(p2)); // test item considered greatest
 	}
-	
+
 	/**
 	 * Tests the following functions in Vector
 	 * 
@@ -161,27 +139,27 @@ public class AITests
 	@Test
 	public void searchNodeTest()
 	{
-		
+
 		// setup
 		SearchNode emptySearchNode = new SearchNode();
-		
-		Point a = new Point(10,10);
-		Point b = new Point(100,100);
+
+		Point a = new Point(10, 10);
+		Point b = new Point(100, 100);
 		SearchNode testSearchNode = new SearchNode(a, emptySearchNode, 20.0, b);
-		
+
 		// distanceTravelled()
 		assertEquals(testSearchNode.distanceTravelled(), 20.0, 0);
-		
+
 		// distanceToGo()
 		double distToGo = StaticHeuristics.euclidean(a, b);
 		assertEquals(testSearchNode.distanceToGo(), distToGo, 0);
-		
+
 		// getLocation()
-		assertEquals(testSearchNode.getLocation(),a);
-		
+		assertEquals(testSearchNode.getLocation(), a);
+
 		// getParent()
 		assertEquals(testSearchNode.getParent(), emptySearchNode);
-		
+
 		// isEmpty()
 		assertTrue(emptySearchNode.isEmpty());
 		assertTrue(!testSearchNode.isEmpty());
