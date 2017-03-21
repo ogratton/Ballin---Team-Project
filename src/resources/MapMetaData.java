593570ef86c46454c4b272f687f61f049ceb6249
oxg558 <oxg558@cs.bham.ac.uk>
2017-03-21 22:13:04 +0000

    added static method that makes a table of modes to compatible maps

--- src/resources/MapMetaData.java
+++ src/resources/MapMetaData.java
@@ -5,6 +5,8 @@ import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Hashtable;
 
 import resources.Resources.Mode;
 
@@ -56,17 +58,18 @@ public class MapMetaData
 	{
 		return name;
 	}
+	
 
 	/**
 	 * Read a map and return the list of gamemodes it can be played on
 	 * If tags occur twice, take the latter occurence (it overwrites)
 	 * 
-	 * @param filename
+	 * @param filepath path to file
 	 * @throws IOException
 	 */
-	public void readMetaData(String filename) throws IOException
-	{
-		br = new BufferedReader(new FileReader(filename));
+	public void readMetaData(String filepath) throws IOException
+	{	
+		br = new BufferedReader(new FileReader(filepath));
 		ArrayList<Mode> compatibleModes = new ArrayList<Mode>();
 
 		while ((line = br.readLine()) != null)
@@ -119,17 +122,46 @@ public class MapMetaData
 		throw new NullPointerException("correspondingMode in MapMetaData called when it shouldn't have been");
 	}
 
-	public static void main(String[] args) throws IOException
+	/**
+	 * Go through every map and adds each to a table
+	 * to build a table of Mode to map name
+	 * e.g. table.get(Mode.HotPotato) will return a set
+	 * of all the maps that can be played on HotPotato
+	 * mode
+	 * 
+	 * @return table of gamemode enum to set of map names (filename - .csv)
+	 * @throws IOException
+	 */
+	public static Hashtable<Mode, HashSet<String>> getTable() throws IOException
 	{
 		MapMetaData mmd = new MapMetaData();
 		File folder = new File(FilePaths.maps);
-		File[] listOfFiles = folder.listFiles();
+		File[] listOfFiles = folder.listFiles();	
+		Hashtable<Mode, HashSet<String>> modeDict = new Hashtable<Mode, HashSet<String>>();
 
 		for (int i = 0; i < listOfFiles.length; i++)
 		{
 			mmd.readMetaData(listOfFiles[i].toString());
-			System.out.println( mmd.getName() + ": " + mmd.getCompatibleModes());
+//			System.out.println( mmd.getName() + ": " + mmd.getCompatibleModes());
+			
+			ArrayList<Mode> compats = mmd.getCompatibleModes();
+			
+			for (int j = 0; j < compats.size(); j++)
+			{
+				Mode tempMode = compats.get(j);
+				HashSet<String> tempSet = modeDict.get(tempMode);
+				if (tempSet == null) tempSet = new HashSet<String>();
+//				String tempFilename = mmd.getFilename();
+				tempSet.add(listOfFiles[i].getName().substring(0, listOfFiles[i].getName().length() - 4));				
+				modeDict.put(tempMode, tempSet);
+			}
 		}
 
+		return modeDict;
+	}
+	
+	public static void main(String[] args) throws IOException
+	{
+		System.out.println(MapMetaData.getTable().get(Mode.HotPotato));
 	}
 }
